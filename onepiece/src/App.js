a31a4e9d113f2ac32f63c5ba8c79ce709b61bfa7
haydentinker <haydentinker613@gmail.com>
2023-05-16 15:22:31 -0700

    Working towards persistent state

--- onepiece/src/App.js
+++ onepiece/src/App.js
@@ -3,41 +3,37 @@ import React, { useState, useRef, useEffect } from 'react';
 import Navbar from "./components/navBar";
 import GuessList from './components/guessList';
 
-const LOCAL_STORAGE_KEY = 'onePieceWordle.guesses'
+const LOCAL_STORAGE_KEY = 'guesses';
+
 function App() {
-  const [guesses, setGuesses] = useState([]);
   const guessRef = useRef();
+  const [guesses, setGuesses] = useState([]);
+
   useEffect(() => {
-    console.log(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)))
     const storedGuesses = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-    if (storedGuesses) setGuesses(storedGuesses); // Replace the current state with the stored guesses
+    if (storedGuesses) setGuesses(storedGuesses);
   }, []);
 
   useEffect(() => {
-
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(guesses));
   }, [guesses]);
 
   function callSearchFunction() {
-    console.log(guessRef.current.value)
     const name = guessRef.current.value;
     setGuesses(prevGuesses => [...prevGuesses, { name: name }]);
     guessRef.current.value = '';
   }
+
   return (
     <>
       <div className="App">
-
         <header>
           <Navbar />
         </header>
         <GuessList guesses={guesses} />
         <input ref={guessRef} type="text" />
         <button onClick={callSearchFunction} type="submit">Guess</button>
-      </div >
-      <body class='yes'>
-
-      </body>
+      </div>
       <footer></footer>
     </>
   );
