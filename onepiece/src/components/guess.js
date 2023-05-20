f79aa9720f5def92dfac8d5f2893323ab451ab8b
haydentinker <haydentinker613@gmail.com>
2023-05-20 16:48:37 -0700

    Character information

--- onepiece/src/components/guess.js
+++ onepiece/src/components/guess.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import '../css/App.css';
-
-
+import luffy from '../images/luffy.png'
 export default function Guess({ guess }) {
-    const mockApicall = { "img_url": "https://static.wikia.nocookie.net/onepiece/images/6/6d/Monkey_D._Luffy_Anime_Post_Timeskip_Infobox.png/revision/latest?cb=20200429191518", "name": "yes", "anime_debut": 450, "affiliations": "Pirates", "occupations": "yes", "birthday": "Jan 10" }
-    const imageUrl = mockApicall.img_url;
-    delete mockApicall.img_url;
+    const mockApicall = { "name": "yes", "anime_debut": 450, "affiliations": "Pirates", "occupations": "yes", "birthday": "Jan 10" }
+    // const imageUrl = mockApicall.img_url;
+    // delete mockApicall.img_url;
     return (
         <>
-            <img src={imageUrl} alt={mockApicall.name} />
+            <img src={luffy} alt={mockApicall.name} className='character_image cardbox' />
             {Object.values(mockApicall).map((value, index) => (
-                <div key={index}>{value}</div>
+                <div className='cardbox' key={index}>{value} </div>
             ))}
         </>
     );
