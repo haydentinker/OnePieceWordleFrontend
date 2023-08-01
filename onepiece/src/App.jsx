99417551e829e81aa394ed487f8e86981778c007
haydentinker <haydentinker613@gmail.com>
2023-08-01 11:49:10 -0700

    Update App.jsx

--- onepiece/src/App.jsx
+++ onepiece/src/App.jsx
@@ -1,14 +1,20 @@
-import {React,useState} from 'react'
+import {React,useState,useEffect} from 'react'
 import { IconButton, TextField, Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@mui/material';
 import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined';
+const LOCAL_STORAGE_KEY='guesses';
 export const App = () => {
-  const [guesses,setGuesses]=useState([]);
+
+  const [guesses,setGuesses]=useState(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || []);
   const [inputValue,setInputValue]=useState('');
 
+  useEffect(()=>{
+    localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(guesses));
+  })
   const handleInputChange =(event)=>{
     setInputValue(event.target.value);
   }
-  const submitGuess =()=>{
+  const submitGuess =(event)=>{
+    event.preventDefault();
     setGuesses([...guesses,inputValue]);
     setInputValue('');
   }
@@ -26,13 +32,14 @@ export const App = () => {
               Guess today's One Piece character!
             </Typography>
             <div style={{ justifyContent: "center", alignItems: "center", display: "flex" }}>
+              <form onSubmit={submitGuess}>
               <TextField value={inputValue} id="outlined-basic" placeholder="Type character name" variant="outlined" size='large' onChange={handleInputChange}
               InputProps={{
                 endAdornment:(
-                  <IconButton onClick={submitGuess}> <ArrowForwardIosOutlinedIcon /></IconButton>
+                  <IconButton type='submit'> <ArrowForwardIosOutlinedIcon /></IconButton>
                 )
               }}/>
-              
+              </form>
               
             </div >
             {guesses &&(
